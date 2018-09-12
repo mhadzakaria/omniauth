a0ea7b2d8ed2c88d7aea42c5619fb99dcb15bffe
Backend9OD <henryss_10@yahoo.com>
2018-09-12 16:08:32 +0700

    update notice

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -38,7 +38,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env['omniauth.auth'])
 
     if @user.persisted?
-      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: request.env['omniauth.auth'].provider.titleize
+      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: request.env['omniauth.auth'].provider.titleize.gsub(' Oauth2', '')
       sign_in_and_redirect @user, event: :authentication
     else
       session['devise.google_data'] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow some session stores
